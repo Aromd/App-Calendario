a59e46916aeafdfeed7239026da7da325c633cb4
Javier Diaz <diazjavierdavid@hotmail.com>
2021-06-09 13:13:15 -0300

    Calendar-app proyecto

---
+++ src/CalendarApp.js
@@ -0,0 +1,14 @@
+import { Provider } from "react-redux";
+import AppRouter from "./router/AppRouter";
+import { store } from "./store/store";
+
+
+const CalendarApp = () => {
+    return (
+        <Provider store={ store } >
+            <AppRouter />
+        </Provider>
+    )
+}
+
+export default CalendarApp;
